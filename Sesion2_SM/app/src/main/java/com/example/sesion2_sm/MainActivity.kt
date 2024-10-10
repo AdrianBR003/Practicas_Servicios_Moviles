0a18e6ee0cb1e6b02e1bec101177aa485fa366a8
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2024-10-10 17:50:51 +0200

    cambiar color a los botones al pulsar

--- Sesion2_SM/app/src/main/java/com/example/sesion2_sm/MainActivity.kt
+++ Sesion2_SM/app/src/main/java/com/example/sesion2_sm/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.sesion2_sm
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,13 +15,47 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.sesion2_sm.ui.theme.Sesion2_SMTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main1)
 
-        val button
+        val buttonLinear = findViewById<Button>(R.id.Linear)
+        val buttonRelative = findViewById<Button>(R.id.Relative)
+        val buttonTable = findViewById<Button>(R.id.Table)
+        val defaultColor = buttonLinear.background
+        val color = ContextCompat.getColor(this, R.color.purple_200) // Opciones de API
+
+        // Metodos para cambiar el color cuando se pulsa
+        setButtonTouchListener(buttonLinear, color, defaultColor)
+        setButtonTouchListener(buttonRelative, color, defaultColor)
+        setButtonTouchListener(buttonTable, color, defaultColor)
+        
+    }
+
+    @SuppressLint("ClickableViewAccessibility") // No se para que sirve la anotacion
+    fun setButtonTouchListener(button: View, color: Int, defaultColor: Drawable) {
+        button.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Cambiar el color cuando se pulsa
+                    button.setBackgroundColor(color)
+                }
+                MotionEvent.ACTION_UP -> {
+                    // Restaurar el color original cuando se suelta
+                    button.background = defaultColor
+                }
+            }
+            true
+        }
+    }
+
+
+
 }
