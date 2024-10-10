e8bd639200befe1910eed06b0cbe72056012ca53
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2024-10-10 18:03:49 +0200

    creacion de las actividades
    Relative_Activity
    Linear_Activity
    Table_Activity

--- Sesion2_SM/app/src/main/java/com/example/sesion2_sm/MainActivity.kt
+++ Sesion2_SM/app/src/main/java/com/example/sesion2_sm/MainActivity.kt
@@ -1,30 +1,20 @@
 package com.example.sesion2_sm
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
-import com.example.sesion2_sm.ui.theme.Sesion2_SMTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main1)
+        setContentView(R.layout.activity_mainconstraint)
 
         val buttonLinear = findViewById<Button>(R.id.Linear)
         val buttonRelative = findViewById<Button>(R.id.Relative)
@@ -36,7 +26,22 @@ class MainActivity : ComponentActivity() {
         setButtonTouchListener(buttonLinear, color, defaultColor)
         setButtonTouchListener(buttonRelative, color, defaultColor)
         setButtonTouchListener(buttonTable, color, defaultColor)
-        
+
+        // Cambiar de Actividad
+        buttonLinear.setOnClickListener {
+            val intent = Intent(this, LinearActivity::class.java)
+            startActivity(intent)
+        }
+        buttonRelative.setOnClickListener {
+            val intent = Intent(this, RelativeActivity::class.java)
+            startActivity(intent)
+        }
+        buttonTable.setOnClickListener {
+            val intent = Intent(this, TableActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
     @SuppressLint("ClickableViewAccessibility") // No se para que sirve la anotacion
@@ -55,6 +60,7 @@ class MainActivity : ComponentActivity() {
             true
         }
     }
+
 
 
 
---
+++ Sesion2_SM/app/src/main/java/com/example/sesion2_sm/RelativeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sesion2_sm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class RelativeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+
+    }
+}
